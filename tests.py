64f7ad8c22b4bf8cf0617b96ebe3fc1585eb3939
Michael Tomkins <michft@gmail.com>
2016-12-15 07:34:36 +0700

    t

---
+++ tests.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import requests
+
+// POST /tasks
+
+// GET /tasks/:taskid
+
+// PUT /tasks/:taskid
+
+// POST /jobs
+
+// GET /jobs/:jobid/status
+
+// GET /jobs/:jobid/output/...path...
+
+
