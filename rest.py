0962ff8bb103e24c3ed9d38bd14214860ff11807
Michael Tomkins <michft@gmail.com>
2016-12-17 21:54:56 +1100

    Method 5

--- rest.py
+++ rest.py
@@ -15,7 +15,7 @@ handled Methods:
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from subprocess import Popen
-import json, os, shutil
+import json, os, psutil, shutil
 
 pids = 1
 jid = 1
@@ -45,22 +45,40 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.send_response(400)
         self.wfile.write(bytes('Something went wrong!\n', 'utf-8'))
 
-    else :
-        #Id = self.path.split('/')
+    elif lst_pth[3] == 'status':
+      try:
+        os.chdir( './' + lst_pth[1] + '/' + lst_pth[2] + '/output')
+        with open ('./pid.txt' , 'r') as fh:
+          st_dict = json.load(fh)
+        try:
+          print(psutil.Process(int(st_dict['pid'])).status)
+          self.send_response(200)
+          self.send_header('Content-type', 'text/json')
+          self.end_headers()
+          self.wfile.write(bytes('{"status" : "running"}\n' , 'utf-8'))
+        except:
+          self.send_response(200)
+          self.send_header('Content-type', 'text/json')
+          self.end_headers()
+          if os.path.getsize('./errors.txt') == 0:
+            self.wfile.write(bytes('{"status" : "success"}\n' , 'utf-8'))
+          else:
+            self.wfile.write(bytes('{"status" : "failed"}\n' , 'utf-8'))
+        os.chdir('../../..')
 
-        os.chdir('./tasks')
-        # Send response status code
-        self.send_response(200)
+      except:
+        self.send_response(400)
+        self.wfile.write(bytes('Something went wrong!\n', 'utf-8'))
+        os.chdir('../../..')
+    elif lst_pth[3] == 'output':
+      try:
+        pass
+      except:
+        pass
 
-        # Send headers
-        self.send_header('Content-type','text/html')
-
-        # Send message back to client
-        message = 'LA ' + os.getcwd()
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, 'utf8'))
-        os.chdir('..')
-
+    else:
+      self.send_response(400)
+      self.wfile.write(bytes('Something went wrong!\n', 'utf-8'))
 
 
   def do_PUT(self):
@@ -105,14 +123,14 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         os.chdir('./tasks')
         os.mkdir('./' + str(pids))
         os.chdir('./' + str(pids))
+
         length = self.headers['content-length']
         data = self.rfile.read(int(length))
-
         with open ('./script.sh', 'w') as fh:
           fh.write(data.decode())
-
         os.chmod('./script.sh' , int(493)) # gets converted to 755 octal
         print(os.getcwd() + '/script.sh written.')
+
         self.send_response(201)
         self.send_header('Content-type', 'text/json')
         self.end_headers()
@@ -125,41 +143,26 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
     elif self.path == '/jobs':
       try:
-# X Decode json, get task ID
-# X change to jobs output Directory
-
         global jid
-
         os.chdir('./jobs')
         os.makedirs('./' + str(jid) + '/output')
         os.chdir('./' + str(jid) + '/output')
+
         length = self.headers['content-length']
         data = self.rfile.read(int(length))
-
-#        print(self.headers['content-type'])
-
-
         job_dict = json.loads(data.decode())
-
-#        print(job_dict['taskid'])
-#        for key, value in job_dict.items():
-#            print(key, value)
-
         shutil.copyfile('../../../tasks/' + str(job_dict['taskid']) + '/script.sh', './script.sh' )
         os.chmod('./script.sh', int(493))
 
-
-# fork term
-#   Set Env VArs in loop.
-#   run script
-#   set pid?
-#   set result?
-        fout = open ('./result' , 'w')
-        p = Popen(['./script.sh'], stdout=fout, env=job_dict['envvars'])
+        fout = open ('./result.txt' , 'w')
+        ferr = open ('./errors.txt' , 'w')
+        p = Popen(['./script.sh'], stdout=fout, stderr=ferr, env=job_dict['envvars'])
         fout.close()
-        fpid = open ('./' + str(p.pid), 'w')
-        fpid.write(str(p.pid))
-        print(str(dict(job_dict['envvars'])))
+        ferr.close()
+        fpid = open ('./pid.txt', 'w')
+        fpid.write('{"pid" : ' + str(p.pid) + "}")
+        fpid.close()
+
         self.send_response(202)
         self.send_header('Content-type', 'text/json')
         self.end_headers()
--- tests.sh
+++ tests.sh
@@ -5,24 +5,28 @@ rm -rf tasks/*  jobs/*
 
 curl -X POST --data '#!/usr/bin/env bash
 
-echo "yeah!"
+echo "yeah!" >> ./output/file.txt
+exit 0
 ' localhost:9876/tasks
 
 curl -X POST --data '#!/usr/bin/env bash
 
+cat ./fileNotHere
 echo "Two"
+exit 0
 ' localhost:9876/tasks
 
 curl -X POST --data '#!/usr/bin/env bash
 
 echo "Three"
+exit 0
 ' localhost:9876/tasks
 
 curl -X POST --data '#!/usr/bin/env bash
 
 echo $VAR1
 echo $VAR2
-sleep 300
+sleep 5
 
 exit 0
 ' localhost:9876/tasks
@@ -51,6 +55,15 @@ curl -X POST --data '{
 
 
 curl -X POST --data '{
+  "taskid" : 2,
+  "envvars" : {
+    "VAR1" : "breaks",
+    "VAR2" : "shell"
+  }
+}
+' localhost:9876/jobs
+
+curl -X POST --data '{
   "taskid" : 4,
   "envvars" : {
     "VAR1" : "Foo",
@@ -59,6 +72,10 @@ curl -X POST --data '{
 }
 ' localhost:9876/jobs
 
+curl -X GET localhost:9876/jobs/1/status
 
+curl -X GET localhost:9876/jobs/2/status
+
+curl -X GET localhost:9876/jobs/3/status
 
 exit 0
